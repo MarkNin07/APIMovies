c8b836c8b9f6f889f1ff52bf02ebc5cd5edd5089
Marcos Nino <tmnino@uninorte.edu.co>
2022-06-04 15:04:17 -0500

    Movie Class
    Movie class and MovieDTO were created on collection and dto packages respectively

--- src/main/java/com/sofkaU/software/demo/collection/Movie.java
+++ src/main/java/com/sofkaU/software/demo/collection/Movie.java
@@ -1,6 +1,20 @@
 package com.sofkaU.software.demo.collection;
 
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+
+@Document(collection = "movie")
+@Data
 public class Movie {
+
+    @Id
+    private String id;
+
+    private String movieName;
+
+    private String synopsis;
+
 }
---
+++ src/main/java/com/sofkaU/software/demo/dto/MovieDTO.java
@@ -0,0 +1,15 @@
+package com.sofkaU.software.demo.dto;
+
+import lombok.Data;
+
+
+@Data
+public class MovieDTO {
+
+    private String id;
+
+    private String movieName;
+
+    private String synopsis;
+
+}
